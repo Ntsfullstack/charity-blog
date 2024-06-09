ba2c55d63b6432c4d955b0ea7b4666f0dc532caa
al1103 <phamtuan72az@gmail.com>
2024-06-09 16:17:08 +0700

    fix footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import './style.scss';
+import React from "react";
+import styles from "./Footer.module.scss";
+import logo from "../../assets/images/collapsedLogo.png";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-content">
-        <p>&copy; 2024 Your Website. All rights reserved.</p>
-        <ul className="footer-links">
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/services">Services</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
+    <footer id="dk-footer" className={styles.Footer}>
+      <div className={styles.copyright}> 
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <div className={styles.colMd6}>
+              <span>Copyright © 2024, All Right Reserved </span>
+            </div>
+            
+          </div>
+        </div>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
